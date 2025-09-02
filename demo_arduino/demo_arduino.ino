fc0902bce32f5f5d64544ab5008cc8990efc6cfa
AndresDFlores <dflores.andres@gmail.com>
2025-09-02 19:58:52 -0400

    updated arduino demo

--- demo_arduino/demo_arduino.ino
+++ demo_arduino/demo_arduino.ino
@@ -5,7 +5,21 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello from Arduino");
-  delay(1000);
+
+  // // put your main code here, to run repeatedly:
+  // Serial.println("Hello from Arduino");
+  // delay(1000);
+
+  // Generate a random number between 0 (inclusive) and 100 (exclusive)
+  long randomNumber1 = random(100); 
+  // Serial.print("Random number (0-99): ");
+  Serial.println(randomNumber1);
+
+  // // Generate a random number between 10 (inclusive) and 50 (exclusive)
+  // long randomNumber2 = random(10, 50); 
+  // Serial.print("Random number (10-49): ");
+  // Serial.println(randomNumber2);
+
+  // delay(1000); // Wait for 1 second before generating the next numbers
+  delay(10);
 }
